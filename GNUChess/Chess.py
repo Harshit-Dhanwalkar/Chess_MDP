5b1e9005ee1e12dbae1d8b12824988a2aedc78df
Phoenix108 <ganesharvindh.r@gmail.com>
2024-03-17 17:38:38 +0530

    Modified Chess.py to remove bug

--- GNUChess/Chess.py
+++ GNUChess/Chess.py
@@ -2,58 +2,65 @@ import chess
 import chess.svg
 import math
 import base64
-from PIL import Image 
 import cv2
 from cairosvg import svg2png
 
 
 def eval_func(x):
-    return math.exp(-(x**2)) # We are taking J(x) = exponential function
+    return math.cos(x)  # We are taking J(x) = exponential function
+
 
 def encode_board(board):
     # Encode the board using base64
-    encoded_board = base64.b64encode(board.board_fen().encode('utf-8')).decode('utf-8')
+    encoded_board = base64.b64encode(board.board_fen().encode("utf-8")).decode("utf-8")
     return encoded_board
+
 
 def decode_board(encoded_board):
     # Decode the encoded board using base64
-    decoded_board_fen = base64.b64decode(encoded_board.encode('utf-8')).decode('utf-8')
+    decoded_board_fen = base64.b64decode(encoded_board.encode("utf-8")).decode("utf-8")
     board = chess.Board(decoded_board_fen)
     return board
 
+
 def base64_to_int(encoded_board):
     bytes_repr = base64.b64decode(encoded_board)
-    int_repr = int.from_bytes(bytes_repr, 'big')
+    int_repr = int.from_bytes(bytes_repr, "big")
     return int_repr
 
 
 def dispBoard(board):
-    f = open("pic.svg","w")
+    f = open("pic.svg", "w")
     a = chess.svg.board(board, flipped=True)
     f.write(a)
     f.close()
     svg2png(url="./pic.svg", write_to="./pic.png")
-    image = cv2.imread('./pic.png')
-    cv2.imshow('image window', image)
+    image = cv2.imread("./pic.png")
+    cv2.imshow("image window", image)
     # cv2.waitKey(1)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         cv2.destroyAllWindows()
+
 
 def simple_terminal_engine():
     # Create a chess board
     board = chess.Board()
+    state_list = [board]
 
-    while not (board.is_checkmate() and board.is_stalemate() and board.is_insufficient_material()):
+    while True:
         final_eval_arr = []
-        A = [i for i in board.legal_moves] #Creating action space
+        A = [i for i in board.legal_moves]  # Creating action space
         for WhiteMove in A:
-            board.push_san(str(WhiteMove)) # This function plays the move and modifies board
+            board.push_san(
+                str(WhiteMove)
+            )  # This function plays the move and modifies board
             temp = []
             for BlackMove in board.legal_moves:
                 board.push_san(str(BlackMove))
+                print(base64_to_int(encode_board(board.fen())))
                 intermediate_eval = eval_func(base64_to_int(encode_board(board)))
                 temp.append(intermediate_eval)
-                board.pop() #Undo the last move
+                board.pop()  # Undo the last move
             final_eval_arr.append(min(temp))
             board.pop()
         final_eval = max(final_eval_arr)
@@ -61,23 +68,31 @@ def simple_terminal_engine():
         board.push_san(str(A[action]))
         print(board)
         dispBoard(board)
+        if board.is_checkmate():
+            state_list.append("W")
+            break
+        if board.is_insufficient_material():
+            state_list.append("D")
+            break
         while True:
             try:
                 PlayerMove = str(input("Enter move : "))
                 board.push_san(PlayerMove)
+                state_list.append(board.fen())
                 break
             except ValueError:
                 print("Invalid move. Please try again.")
                 board.pop()
         print(board)
         dispBoard(board)
-simple_terminal_engine()
+        if board.is_checkmate():
+            state_list.append("L")
+            break
+        if board.is_insufficient_material():
+            state_list.append("D")
+            break
+
+    return state_list
 
 
-
-
-
-
-                
-            
-
+l = simple_terminal_engine()
